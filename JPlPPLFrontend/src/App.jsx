86c60e14d8c2502c6df49f67d8655cfa2061c93b
Nishant Jhade <jhadenishant@gmail.com>
2024-12-28 19:59:11 +0530

    quize section added

--- JPlPPLFrontend/src/App.jsx
+++ JPlPPLFrontend/src/App.jsx
@@ -9,11 +9,12 @@ import Cursor from "./Componentes/Cursor/Cursor";
 import './Componentes/Cursor/Cursor.css';
 // import Compiler from "./Componentes/Compiler/CodeEditor";
 import Addqueform from "./Componentes/Admin/Addque/Addqueform";
-import Contestlogin from "./Componentes/Contest/Contestlogin/Contestlogin";
 import ContestHomepage from "./Componentes/Contest/ContestHome/ContestHomepage";
 import AdminLogin from "./Componentes/Admin/Adminlogin/Adminlogin";
 import AdminHomepage from "./Componentes/Admin/Adminhome/Adminhomepage";
 import Allquestions from "./Componentes/Admin/ViewQue/AllQuestions";
+import ContestQuize from "./Componentes/Contest/ContestQuize/ContestQuize";
+import ContestLogin from "./Componentes/Contest/ContestLogin/Contestlogin";
 
 
 const App = () => {
@@ -29,13 +30,13 @@ const App = () => {
           <Route path="/" element={<Homepage />} />
           <Route path="/contact" element={<ContactUs />} />
           <Route path="/register" element={<Regiter />} />
-          <Route path="/contestlogin" element={<Contestlogin/>} />
+          <Route path="/contestlogin" element={<ContestLogin/>} />
           <Route path="/contesthomepage" element={<ContestHomepage/>} />
           <Route path="/add-question" element={<Addqueform/>} />
           <Route path="/allquestions" element={<Allquestions/>} />
           <Route path="/adminlogin" element={<AdminLogin/>} />
           <Route path="/adminhomepage" element={<AdminHomepage/>} />
-          
+          <Route path="/contestquize" element={<ContestQuize/>} />
         </Routes>
       <Footer/>
       </Router>
--- JPlPPLFrontend/src/Componentes/Contest/ContestQuize/ContestQuize.jsx
+++ JPlPPLFrontend/src/Componentes/Contest/ContestQuize/ContestQuize.jsx
@@ -1,83 +1,81 @@
-import React, { useRef, useState } from 'react'
-import "./quize.css"
-import { data } from '../assets/data'
-const quize = () => {
-    let [index,setIndex] = useState(0);
-    let [question,setQuestion] = useState(data[index]);
-    //use data from data set
-    let [lock,setLock] = useState(false);
-    let [score,setScore] = useState(0);
-    let [result,setResult]= useState(false)
+import React, { useRef, useState } from 'react';
+import "./ContestQuize.css";
+import { data } from '../../../assets/API/data';
+
+const ContestQuize = () => {
+    let [index, setIndex] = useState(0);
+    let [question, setQuestion] = useState(data[index]);
+    let [lock, setLock] = useState(false);
+    let [score, setScore] = useState(0);
+    let [result, setResult] = useState(false);
 
     let option1 = useRef(null);
     let option2 = useRef(null);
     let option3 = useRef(null);
     let option4 = useRef(null);
-    
-    let option_arr = [option1,option2,option3,option4];
 
-    
-    const checkAns =(e,ans)=>{
-        if(lock === false)
-        if (question.ans===ans){
-            e.target.classList.add("correct")
-            setLock(true)
-            setScore(prev=>prev+1)
+    let option_arr = [option1, option2, option3, option4];
+
+    const checkAns = (e, ans) => {
+        if (lock === false) {
+            if (question.ans === ans) {
+                e.target.classList.add("contestquize-correct");
+                setLock(true);
+                setScore(prev => prev + 1);
+            } else {
+                e.target.classList.add("contestquize-wrong");
+                setLock(true);
+                option_arr[question.ans - 1].current.classList.add("contestquize-correct");
+            }
         }
-        else{
-            e.target.classList.add("wrong")  
-            setLock(true)
-            option_arr[question.ans-1].current.classList.add("correct")
-        }
-    }
-    const next =()=>{
-        if (lock===true){
-            if(index === data.length-1){
+    };
+
+    const next = () => {
+        if (lock === true) {
+            if (index === data.length - 1) {
                 setResult(true);
                 return 0;
             }
             setIndex(++index);
             setQuestion(data[index]);
             setLock(false);
-            option_arr.map((option)=>{
-                option.current.classList.remove("wrong");
-                option.current.classList.remove("correct");
+            option_arr.map((option) => {
+                option.current.classList.remove("contestquize-wrong");
+                option.current.classList.remove("contestquize-correct");
                 return null;
-            })
+            });
         }
-    }
-    const reset = ()=>{
+    };
+
+    const reset = () => {
         setIndex(0);
         setQuestion(data[index]);
         setLock(false);
         setScore(0);
         setResult(false);
-    }
-  return (
-      <div className='cantainer'>
-      <h1>Quize app</h1>
-        <hr />
-        {result?<></>:<> 
-        <h2>{index+1}.{question.question}</h2>
-        <ul>
-         
-        <li ref={option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
-        <li  ref={option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
-        <li ref={option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
-        <li ref={option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
-        
-        </ul>
-        <button onClick={next} >Next</button>
-        <div className="index">{index+1} of {data.length} question</div></>}
-        {result?<>
-        <h1 id='score' >you  scored {score} out of {data.length}</h1>
-        <button onClick={reset} >Reset</button></>:<></>}
-        
-      </div>
-       
-        
-       
-  )
-}
+    };
 
-export default quize
+    return (
+        <div className='contestquize-container'>
+            <h1>Quiz App</h1>
+            <hr />
+            {result ? <></> : <>
+                <h2>{index + 1}. {question.question}</h2>
+                <ul>
+                    <li ref={option1} onClick={(e) => { checkAns(e, 1) }}>{question.option1}</li>
+                    <li ref={option2} onClick={(e) => { checkAns(e, 2) }}>{question.option2}</li>
+                    <li ref={option3} onClick={(e) => { checkAns(e, 3) }}>{question.option3}</li>
+                    <li ref={option4} onClick={(e) => { checkAns(e, 4) }}>{question.option4}</li>
+                </ul>
+                <button onClick={next}>Next</button>
+                <div className="contestquize-index">{index + 1} of {data.length} questions</div>
+            </>}
+            {result ? <>
+                <h1 id='contestquize-score'>You scored {score} out of {data.length}</h1>
+                <button onClick={reset}>Reset</button>
+            </> : <></>}
+        </div>
+    );
+};
+
+export default ContestQuize;
