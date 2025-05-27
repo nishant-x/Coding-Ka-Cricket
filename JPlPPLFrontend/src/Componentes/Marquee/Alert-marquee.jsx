48df50920e9c44ef4ff13c66f3617d6f099bb0c0
Nishant Jhade <jhadenishant@gmail.com>
2025-05-27 20:02:20 +0530

    Alert Added

--- JPlPPLFrontend/src/Componentes/Marquee/Alert-marquee.jsx
+++ JPlPPLFrontend/src/Componentes/Marquee/Alert-marquee.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import './Alert-marquee.css'
+import './Alert-marquee.css';
+
 function Alertmarquee() {
   return (
     <div className="Alert-News">
-      <marquee width="100%" direction="left" scrollamount="5">
-        <h3>🚨 Breaking News: This is a sample scrolling text that scrolls to the left! 🚨 
-        Stay tuned for updates! 🌍</h3>
+      <marquee width="100%" direction="left" scrollamount="10">
+        <h3>🚨 URGENT: Registration closing soon! Final deadline approaching! 🚨 
+        Register now before it's too late! 🚀 
+        Last chance to secure your spot - Don't miss out! ⏳ 
+        </h3>
       </marquee>
     </div>
   );
 }
-  
+
 export default Alertmarquee;
