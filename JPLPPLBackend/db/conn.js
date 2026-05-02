0f618dbda6669a4a01fd706de4dcd36730ddea71
Nishant Jhade <168710192+nishant-x@users.noreply.github.com>
2026-05-02 13:29:28 +0530

    Remove Express server setup from conn.js
    Removed Express server initialization from database connection file.

--- JPLPPLBackend/db/conn.js
+++ JPLPPLBackend/db/conn.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
-const express = require('express');
-const app= express();
 global.crypto = crypto;
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGOOSE_URL)
   .then(() => {
     console.log('Connected to MongoDB');
-
-    app.listen(process.env.PORT || 5000, () => {
-      console.log('Server running');
-    });
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err);
