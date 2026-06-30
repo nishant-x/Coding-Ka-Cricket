a807c5dec43e76e78388b0be1582531a586cecc1
Nishant Jhade <168710192+nishant-x@users.noreply.github.com>
2026-06-30 12:04:59 +0530

    Update DeveloperCards.jsx

--- JPlPPLFrontend/src/Componentes/DeveloperCards/DeveloperCards.jsx
+++ JPlPPLFrontend/src/Componentes/DeveloperCards/DeveloperCards.jsx
@@ -92,7 +92,7 @@ const DeveloperCards = () => {
           </div>
         </div>
       ) : (
-        <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-4">
+        <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3">
           {cards.map((card) => (
             <DeveloperCard key={card.name} card={card} />
           ))}
